38f74bb09c4c9aa1850a7e7a1e44beed1ca351fc
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2025-09-22 22:23:42 +0700

    ex_12.py

--- ex_12.py
+++ ex_12.py
@@ -1,1 +1,16 @@
-
+import keyword
+name = input()
+if keyword.iskeyword(name):                        # Kлючевое слово
+    print("Нет")
+if name[0].isdigit():                              # Цифра
+    print("Нет")
+else:
+    flag = 1
+    for index in name:
+        if not (index.isalnum() or index == '_'):  # Буква или цифра
+            flag = 0
+            break
+    if flag == 1:
+        print("Да")
+    else:
+        print("Нет")
