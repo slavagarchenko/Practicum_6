fcf8dc55ee35a14af986fdbca51f43a37f325972
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2025-09-22 22:34:40 +0700

    ex_13.py

--- ex_13.py
+++ ex_13.py
@@ -1,1 +1,15 @@
-
+count = 0
+while True:
+    ticket = input()
+    count += 1
+    n = len(ticket)
+    if n % 2 != 0:
+        continue
+    polovina = n // 2
+    first = ticket[:polovina]
+    second = ticket[polovina:]
+    sum_1 = sum(int(index) for index in first)
+    sum_2 = sum(int(index) for index in second)
+    if sum_1 == sum_2:
+        print(count)
+        break
