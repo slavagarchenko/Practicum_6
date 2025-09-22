17263e38fc8b1c23a8eab155d6816973e5dd6938
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2025-09-22 22:25:08 +0700

    ex_12.py

--- ex_12.py
+++ ex_12.py
@@ -1,13 +1,13 @@
 import keyword
 name = input()
-if keyword.iskeyword(name):                        # Kлючевое слово
+if keyword.iskeyword(name):                        # key world
     print("Нет")
-if name[0].isdigit():                              # Цифра
+if name[0].isdigit():                              # The number
     print("Нет")
 else:
     flag = 1
     for index in name:
-        if not (index.isalnum() or index == '_'):  # Буква или цифра
+        if not (index.isalnum() or index == '_'):  # Letter or number
             flag = 0
             break
     if flag == 1:
